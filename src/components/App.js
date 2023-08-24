e5a6ddeb202e97b35fd9cc733fbd09fdef188a01
Idalis Valadez <idalisvaladez22@gmail.com>
2023-08-23 19:20:54 -0500

    Complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react"; // import useState hook
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
@@ -6,13 +6,25 @@ function App() {
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const [isDarkMode, setIsDarkMode] = useState(false) 
+  // create a variable to represent current value and a setter function to dynamically change it
+
+  // create that handler function and utilize our setter function setIsDarkMode to change the state of isDarkMode
+  // since we want it to toggle, add a bang operator do have the variable set to the opposite of what isDarkMode current value
+  function handleMode() {
+  setIsDarkMode(!isDarkMode) 
+}
 
   return (
-    <div className={appClass}>
+    <div className={isDarkMode ? "App dark" : "App light"} > 
+    {/* update the actual browser to represent the Ligh and Dark version. Referencing the class name of our css 
+    call our state variable, if isDarkMode true then we want the app dark else light app */}
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleMode}>{isDarkMode ? "Dark Mode" : "Light Mode"}</button> 
+        {/* call a handler function that will take care of toggle between light and dark mode */}
+        {/* now update the text in the button by using the state variable value. If true we want it to say
+        dark mode else light mode */}
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, {useState} from "react"; // import useState Hook to use
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+const [selectedCategory, setSelectedCatergory] = useState("All") // create variable and setter function to change the category
+// set variauble state to All starting off
+
+function handleChange(event) { // create a handleChange function that listens to our change event and gets that value
+  // set that value into our setter funtion to keep track of the selected input value
+  setSelectedCatergory(event.target.value)
+}
+
+const filteredItems = items.filter((item) => { // we need to filter through our items list in order to know which 
+  //selectedCategory matches and return it if its === to the item.category
+  if (selectedCategory === "All") {
+    return true
+  } else {
+    return item.category === selectedCategory
+  }
+})
+
+
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleChange}> 
+        {/* call onChange event bc it handles changes to input values, and call a handle change function */}
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +34,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredItems.map((item) => ( //instead of just .map through items we want this to .map through the filteredItems
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
